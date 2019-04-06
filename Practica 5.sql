1c0b3f45ee2e719bae1e2af3f60910d0069389f2
Killerernesto <47698348+Killerernesto@users.noreply.github.com>
2019-04-05 21:48:56 -0600

    Add files via upload

---
+++ Practica 5.sql
@@ -0,0 +1,141 @@
+use Muebleria
+ select * from Productos
+insert into Almacen (Id_Almacen,Id_direccion,Id_Proveedor)values(655,4561,8451)
+insert into Almacen (Id_Almacen,Id_direccion,Id_Proveedor)values(5221,2388,35423)
+insert into Almacen (Id_Almacen,Id_direccion,Id_Proveedor)values(822,753,2335)
+insert into Almacen (Id_Almacen,Id_direccion,Id_Proveedor)values(228,786,5252)
+insert into Almacen (Id_Almacen,Id_direccion,Id_Proveedor)values(6261,8765,555)
+
+insert into Proveedor(Id_Proveedor)values(741)
+insert into Proveedor(Id_Proveedor)values(855)
+insert into Proveedor(Id_Proveedor)values(963)
+insert into Proveedor(Id_Proveedor)values(321)
+insert into Proveedor(Id_Proveedor)values(456)
+insert into Proveedor(Id_Proveedor)values(987)
+insert into Proveedor(Id_Proveedor)values(789)
+insert into Proveedor(Id_Proveedor)values(753)
+insert into Proveedor(Id_Proveedor)values(951)
+insert into Proveedor(Id_Proveedor)values(486)
+
+insert into Compra(Id_compra,Id_Almacen,Id_Proveedor,Id_Producto,Pago)values(486,28013,84651,9846,8452)
+insert into Compra(Id_compra,Id_Almacen,Id_Proveedor,Id_Producto,Pago)values(725,342354,84651,98,741)
+insert into Compra(Id_compra,Id_Almacen,Id_Proveedor,Id_Producto,Pago)values(42354,84564,4348,2525,538)
+insert into Compra(Id_compra,Id_Almacen,Id_Proveedor,Id_Producto,Pago)values(55428,4524534,84651,8244,2568)
+insert into Compra(Id_compra,Id_Almacen,Id_Proveedor,Id_Producto,Pago)values(5839342,4534534,25258,9846,123)
+insert into Compra(Id_compra,Id_Almacen,Id_Proveedor,Id_Producto,Pago)values(254864,4525254,84651,9846,453)
+insert into Compra(Id_compra,Id_Almacen,Id_Proveedor,Id_Producto,Pago)values(2542,2434532,25425,9846,8969)
+insert into Compra(Id_compra,Id_Almacen,Id_Proveedor,Id_Producto,Pago)values(2582,8678687,8384651,98461,205)
+insert into Compra(Id_compra,Id_Almacen,Id_Proveedor,Id_Producto,Pago)values(23925,7865,3642,2543,2568)
+
+
+insert into Cliente (Id_Cliente,nombre,Appaterno,ApMaterno,telefono)values(4532,'alfonso','rodriguez','alonso',12345567)
+insert into Cliente (Id_Cliente,nombre,Appaterno,ApMaterno,telefono)values(741,'pepe','rogriguez','alfonso',43346656)
+insert into Cliente (Id_Cliente,nombre,Appaterno,ApMaterno,telefono)values(852,'alfonso','ramos','perez',6536888)
+insert into Cliente (Id_Cliente,nombre,Appaterno,ApMaterno,telefono)values(963,'eduardo','alfonso','zuñiga',6766645)
+insert into Cliente (Id_Cliente,nombre,Appaterno,ApMaterno,telefono)values(321,'pedro','felipe','torres',345678987)
+insert into Cliente (Id_Cliente,nombre,Appaterno,ApMaterno,telefono)values(753,'alfonso','hernandez','topetiux',9876543)
+insert into Cliente (Id_Cliente,nombre,Appaterno,ApMaterno,telefono)values(951,'mauricio','el','aczino',1234567)
+
+
+
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(500,'Closet','Closet color cafe',3000)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(357,'Sillon','Sillon color negro',1000)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(385,'Banca','Bancas color cafe obscuro',1000)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(392,'Percheros','Percheros color cafe',500)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(534,'POOF','POOF color cafe',1500)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(365,'Periqueras Para Bebe','Periqueras Para Bebe color blacno',2000)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(398,'Mesa de madera','Mesa de madera color cafe obscuro',3000)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(285,'Silla de exterior','Silla de exterior color cafe obscuro',1000)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(2963,'Barras para bares','Barras para bares color cafe obscuro',400)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(251,'Bancos','Bancos color cafe',500)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(852,'Bases de madera','Bases de madera color cafe',3000)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(951,'stands','stands color negro',2000)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(753,'Perfumero','Perfumero color cafe obscuro',1700)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(850,'Peceras','Pecera 4 metros cuadrados',900)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(7420,'buzones','buzones color blanco',2500)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(282,'Portarretratos','Portarretratos color cafe',200)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(232,'Señalamientos','Señalamientos decorativos',300)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(175,'Mueble escolares','Mueble escolares colorblanco',2000)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(287,'mueble de laboratorio','mueble de laboratorio blanco',300)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(075,'porta zapatos','porta zapatos color azul',600)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(4385,'Escritorio','Escritorio color cafe',1200)
+
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(6512,'ropero','ropero color cafe claro',2500)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(98746,'Puerta','Puerta de madera color blanco',300)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(2156,'Puerta','Puerta de madera color cafe',300)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(98745,'Puerta','Puerta de madera color cafe obscuro',300)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(5612,'Puerta','Puerta de madera color cafe calro',300)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(520875,'Puerta','Puerta de madera color crema ',300)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(5727,'Puerta','Puerta de madera color negro',300)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(5527588,'Mesa de cocina','mesa de cocina color cafe',600)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(92529,'Silla de cocina','Silla de cocina color cafe',1200)
+insert into productos(Id_Producto,Nombre_Producto,Descripcion,Precio_Producto)values(75530,'mesa de centro','mesa de centro color cafe',2500)
+
+
+
+select * from productos
+
+insert into Empleados(Id_Empleado,nombre,Appaterno,ApMaterno,fechanac,telefono)values(600,'Alejandro','batres','Herrera','9/12/1998',81125465)
+insert into Empleados(Id_Empleado,nombre,Appaterno,ApMaterno,fechanac,telefono)values(601,'silerio','esquivel','ronaldo','9/12/1998',81125465)
+insert into Empleados(Id_Empleado,nombre,Appaterno,ApMaterno,fechanac,telefono)values(602,'ernesto','gonzalez','mitsui','9/12/1998',81125465)
+insert into Empleados(Id_Empleado,nombre,Appaterno,ApMaterno,fechanac,telefono)values(603,'mauricio','lopez','rodriguez','9/12/1998',81125465)
+insert into Empleados(Id_Empleado,nombre,Appaterno,ApMaterno,fechanac,telefono)values(604,'osmar','gutierres','macias','9/12/1998',81125465)
+insert into Empleados(Id_Empleado,nombre,Appaterno,ApMaterno,fechanac,telefono)values(605,'allfonso','herrera','lopez','9/12/1998',81125465)
+insert into Empleados(Id_Empleado,nombre,Appaterno,ApMaterno,fechanac,telefono)values(606,'fernando','milos','bolaños','9/12/1998',81125465)
+
+
+
+insert into direciones(Id_direccion,colonia,numero)values(275,'Jardines de anahuac',8452)
+insert into direciones(Id_direccion,colonia,numero)values(5275,'real de anahuac',2352)
+insert into direciones(Id_direccion,colonia,numero)values(7753,'azteca',8632)
+insert into direciones(Id_direccion,colonia,numero)values(7522,'langrage',7531)
+insert into direciones(Id_direccion,colonia,numero)values(5375,'futuro nogalar',7842)
+insert into direciones(Id_direccion,colonia,numero)values(7352,'año de juarez',1452)
+insert into direciones(Id_direccion,colonia,numero)values(7522,'santo domingo',4536)
+
+
+
+
+
+
+
+insert into Sucursales (Id_Sucursal,Direccion,telefono)values(520,'fome la paloma',741258)
+insert into Sucursales (Id_Sucursal,Direccion,telefono)values(2561,'granja ocaso',789654123)
+insert into Sucursales (Id_Sucursal,Direccion,telefono)values(951,'acero palo alto',12344)
+insert into Sucursales (Id_Sucursal,Direccion,telefono)values(124,'san carlos lincoln',7895162)
+insert into Sucursales (Id_Sucursal,Direccion,telefono)values(349,'dos rios colosio',85214785)
+
+
+insert into Ticket(Id_Ticket,Id_Sucursal,Id_Venta,Pago_Venta)values(852,6456,5645,500)
+insert into Ticket(Id_Ticket,Id_Sucursal,Id_Venta,Pago_Venta)values(7564,8456,4564,64)
+insert into Ticket(Id_Ticket,Id_Sucursal,Id_Venta,Pago_Venta)values(45645,45,64564,564)
+insert into Ticket(Id_Ticket,Id_Sucursal,Id_Venta,Pago_Venta)values(42578,7546,13412,56456)
+insert into Ticket(Id_Ticket,Id_Sucursal,Id_Venta,Pago_Venta)values(864584,456,84648,4568)
+
+insert into venta(Id_Venta,Id_Producto,Id_Empleado,Dia_Venta,Id_Cliente,Costo_Venta)values(515,45323,3,'10/10/2019',800,369)
+insert into venta(Id_Venta,Id_Producto,Id_Empleado,Dia_Venta,Id_Cliente,Costo_Venta)values(9898,501,455343,'10/01/2019',801,963)
+insert into venta(Id_Venta,Id_Producto,Id_Empleado,Dia_Venta,Id_Cliente,Costo_Venta)values(3614,45387,738753,'10/01/2019',802,369)
+insert into venta(Id_Venta,Id_Producto,Id_Empleado,Dia_Venta,Id_Cliente,Costo_Venta)values(2857,67453,2435,'10/01/2019',803,963)
+insert into venta(Id_Venta,Id_Producto,Id_Empleado,Dia_Venta,Id_Cliente,Costo_Venta)values(728,254,8,'10/01/2019',804,159)
+
+update Productos set Precio_Producto = 250 where Id_Producto = 500
+update Productos set Precio_Producto = 36592 where Id_Producto = 357
+update Productos set Precio_Producto = 5244 where Id_Producto = 232
+update Productos set Precio_Producto = 4452 where Id_Producto = 520875
+update Productos set Precio_Producto = 25752 where Id_Producto = 2963
+update Productos set Precio_Producto = 25857 where Id_Producto = 75530
+update Productos set Precio_Producto = 3645 where Id_Producto = 5527588
+update Productos set Precio_Producto = 2551 where Id_Producto = 92529
+update Productos set Precio_Producto = 95123 where Id_Producto = 98746
+update Productos set Precio_Producto = 123775 where Id_Producto = 075
+ 
+delete from Productos where Id_Producto = 500
+delete from Productos where Id_Producto = 357
+delete from Productos where Id_Producto = 232
+delete from Productos where Id_Producto = 520875
+delete from Productos where Id_Producto = 2963
+delete from Productos where Id_Producto = 75530
+delete from Productos where Id_Producto = 5527588
+delete from Productos where Id_Producto = 92529
+delete from Productos where Id_Producto = 98746
+delete from Productos where Id_Producto = 075
